49cf7b1514527a015371f2b100838d3bffa64c0a
lgp <lgp@qq.com>
2018-09-10 17:09:37 +0800

    add swagger support

--- schoolMonitor/monitor-main/src/main/java/com/lgp/monitor/config/AppConfiguration.java
+++ schoolMonitor/monitor-main/src/main/java/com/lgp/monitor/config/AppConfiguration.java
@@ -1,13 +1,57 @@
 package com.lgp.monitor.config;
 
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @ComponentScan(basePackages = {
 		"com.lgp.monitor.schedule",
-		"com.lgp.monitor.service"
+		"com.lgp.monitor.service",
+		"com.lgp.monitor.controller"
 })
 public class AppConfiguration {
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public Docket docketCommon() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Monitor").select()
+				.apis(RequestHandlerSelectors.basePackage("com.lgp.monitor.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().
+						title("Restful API").
+						description("接口的API").
+						contact(new Contact("LGP", "", "")).
+						version("1.0").
+						build());
+	}
+	
+	@Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("10MB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
 	
 }
