1df72f38c5eb27e2a2b6e3dea79763c0a804af65
lgp <lgp@qq.com>
2018-10-17 14:57:44 +0800

    添加一个翻译功能工程

---
+++ schoolMonitor/ai-translate/src/com/my/ai/Main.java
@@ -0,0 +1,18 @@
+package com.my.ai;
+
+import com.my.ai.translate.TransApi;
+
+public class Main {
+
+    // 在平台申请的APP_ID 详见 http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    private static final String APP_ID = "20181016000220072";
+    private static final String SECURITY_KEY = "rDYSK0y8kii9m9m5CVzk";
+
+    public static void main(String[] args) {
+        TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+
+        String query = "高度600米";
+        System.out.println(api.getTransResult(query, "auto", "en"));
+    }
+
+}
