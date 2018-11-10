e5b79ae7ce945ddf7c22410f398c82850d4be736
lgp <lgp@qq.com>
2018-11-10 19:00:19 +0800

    获取词组集合

--- schoolMonitor/common/src/main/java/com/my/service/BaiduAipNlp.java
+++ schoolMonitor/common/src/main/java/com/my/service/BaiduAipNlp.java
@@ -1,7 +1,10 @@
 package com.my.service;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.baidu.aip.nlp.AipNlp;
@@ -27,7 +30,22 @@ public class BaiduAipNlp {
         		+ "本以来他会好好火几年，没想到娱乐圈更新换代的速度太快，阿杜的“火”到底成了过去式。"
         		+ "视频里的阿杜虎背熊腰，早已没有了当年的清爽模样，不过他也人到中年，发福长胖在所难免。";
         JSONObject res = client.lexer(text, options);
-        System.out.println(res.toString(2));
+        //System.out.println(res.toString(2));
+        JSONArray items = res.getJSONArray("items");
+        Set<String> cis = new HashSet<String>();
+        for (int i = 0; i < items.length(); i++) {
+			JSONObject item = items.getJSONObject(i);
+			String item_text = item.getString("item");
+			if(item_text.length() > 1 & item_text.length() < 5) {
+				//System.out.println(item_text);
+				cis.add(item_text);
+			}
+		}
+        //System.out.println(text.length());
+        System.out.println(cis.size());
         
+        for (String ci : cis) {
+			System.out.println(ci);
+		}
     }
 }
